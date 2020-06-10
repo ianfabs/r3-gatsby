996022cbf75b5587298deae22820b76b1a3a3f37
Ian Fabs <ian@fabs.dev>
2020-06-10 16:05:31 -0400

    Started work on Youtube video API implementation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+const ChannelID = "UC46zRme2Bk0iCdhT-3J-Sjw";
+const APIKey = "AIzaSyCcQioG7YFXwKykkv1QIPr4eUdEuoIBYAs";
+const APIEndpoint = `https://www.googleapis.com/youtube/v3/search?key=${APIKey}&channelId=${ChannelID}&part=snippet,id&order=date`;
+
 exports.onCreateNode = ({ node }) => {
   console.log(node.internal.type)
 }
