1f1934ad7aebd84552fcd5b750c7b110ccfb8e1f
Ian Fabs <ianfabs@outlook.com>
2020-06-20 05:49:34 -0400

    Added future code in comment

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+/* 
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components"
+
+const Footer = styled.footer`
+  background: hsla(0,0%,0%,0.9);
+  padding: 2vmin 8vmin;
+  font-weight: 500;
+  color: white;
+` */
+
+// Figure out how to get footer to render at the bottom of the body
