fe9d036cd39e33ea40c9ba1520d77b432b1a0370
Ian Fabs <ianfabs@outlook.com>
2020-06-20 05:50:58 -0400

    Corrected intrinsic element props

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -4,9 +4,9 @@ export default ({vid, link}) => {
   return (
     <iframe
       src={`https://www.youtube-nocookie.com/embed/${link ? link : vid}`}
-      frameborder="0" 
+      frameBorder="0" 
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-      allowfullscreen
+      allowFullScreen
       title="Unique"
       style={{width: "100%", height: "30vmax"}}
     >
