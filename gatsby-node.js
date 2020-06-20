e1228934d33878f1c2bed3a060ffe1a14cfe47f6
Ian Fabs <ianfabs@outlook.com>
2020-06-20 05:49:42 -0400

    Removed unused code

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,3 @@
-const {google} = require('googleapis');
-const {authenticate} = require('@google-cloud/local-auth');
-const path = require('path');
-
-const ChannelID = "UC46zRme2Bk0iCdhT-3J-Sjw";
-const APIKey = "AIzaSyDVf4Ym29FYv18xQlsaeYtyF3F8jwNQv5Y";
-
 exports.onCreateNode = ({ node }) => {
   // console.log(node.internal.type)
-}
-
-let yt = google.youtube('v3');
-
-exports.createPages = async ({ actions: { createPage } }) => {
-
-  /* createPage({
-    path: `/videos/`,
-    component: require.resolve("./src/templates/videos.jsx"),
-    context: { allVideos },
-  }) */
 }
